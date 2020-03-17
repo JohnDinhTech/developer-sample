3c111663587cf5386e994de4815f78d527d7fcaf
John Dinh <johnduydinh@gmail.com>
2020-03-17 15:10:51 -0400

    Updates

---
+++ precache-manifest.82bda92775297f28bda0e1daac6127f1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df146f3361935b0fe6d4ac367fdd2d03",
+    "url": "/developer-sample/index.html"
+  },
+  {
+    "revision": "4efad2505bd930b84276",
+    "url": "/developer-sample/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "856640e43583a01a7344",
+    "url": "/developer-sample/static/css/main.27b5ac59.chunk.css"
+  },
+  {
+    "revision": "4efad2505bd930b84276",
+    "url": "/developer-sample/static/js/2.870411ef.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/developer-sample/static/js/2.870411ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856640e43583a01a7344",
+    "url": "/developer-sample/static/js/main.f711a910.chunk.js"
+  },
+  {
+    "revision": "3f45edd1da766f716efa",
+    "url": "/developer-sample/static/js/runtime-main.02b170a2.js"
+  },
+  {
+    "revision": "cc949a62a5c04e90afee4935841f6802",
+    "url": "/developer-sample/static/media/lesson1-card.cc949a62.jpg"
+  },
+  {
+    "revision": "dd303e525c150c3d399c58c494a9e843",
+    "url": "/developer-sample/static/media/lesson2-card.dd303e52.jpg"
+  },
+  {
+    "revision": "905385cf054cfb9aa78f61571c16b3ef",
+    "url": "/developer-sample/static/media/lesson3-card.905385cf.jpg"
+  },
+  {
+    "revision": "b9d90ab97ae0739045c2bd4c4403c8a7",
+    "url": "/developer-sample/static/media/slide1.b9d90ab9.jpg"
+  },
+  {
+    "revision": "f3766e774ec39650da3d540d486e41f0",
+    "url": "/developer-sample/static/media/slide2.f3766e77.png"
+  },
+  {
+    "revision": "9b7d0da2e39127568c48ce1892326e66",
+    "url": "/developer-sample/static/media/slide3.9b7d0da2.jpg"
+  },
+  {
+    "revision": "e2edb601c99dfb1b004d989bd3e94342",
+    "url": "/developer-sample/static/media/slide4.e2edb601.jpg"
+  },
+  {
+    "revision": "5a999003b1bf8aa5a978ffedafcc7df0",
+    "url": "/developer-sample/static/media/slide5.5a999003.jpg"
+  }
+]);
